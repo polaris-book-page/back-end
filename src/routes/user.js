f2d4e28d78fe4ed536a6920ffa94e16e232b5ec7
kjh073 <9279482@naver.com>
2023-07-28 06:44:05 +0900

    [feat] Implement login feature - set up session & cookie info - check a id &  password is correct

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,14 +5,27 @@ const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const { User } = require('../models/model') 
 const mongoose = require('mongoose')
+const config = require('../../config/key');
+const session_key = require('../../config/session_key');
+const MongoStore = require("connect-mongo");
 const bcrypt = require('bcrypt')
 const saltRounds = 10
 
 app.use(cookieParser())
 app.use(session({
-    secret: '1242',
+    httpOnly: true,
+    secure: true,
+    secret: `${ session_key.secret_key }`,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        secure: true,
+        maxAge:(3.6e+6)*24 // 24시간 유효
+    },
+    store: MongoStore.create({
+        mongoUrl: config.mongoURI
+    })
 }))
 
 router.post('/join', async (req, res) => {
@@ -71,10 +84,34 @@ router.post('/initial-evaluation', (req, res) => {
     res.send("/initial-evaluation");
 });
 
-router.post('/login', (req, res) => {
-    res.send("login");
-});
-
+router.post('/login', async (req, res) => {
+    const { _id, password } = req.body;
+    if (!req.session) {
+        req.session = {};
+    }
+    try {
+        const user = await User.findOne({ _id });
+        if (!user) {
+            return res.json({ existingUser: false });
+        }
+    
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.json({ loginSuccess: false });
+        }
+    
+        req.session.user = {
+            id: _id,
+            is_logined: true,
+        };
+        console.log(req.session);
+        return res.json({ loginSuccess: true, session: req.session });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'An error occurred during login.' });
+    }
+  });
+  
 router.post('/subscribe', (req, res) => {
     res.send("join");
 });

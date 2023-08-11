3ef80072f696ab67d7a5c8d745a571160069b7ca
otcroz <dbtndus0527@gmail.com>
2023-08-11 18:08:56 +0900

    [feat] Implement add book API - Implement Add book API - Modify S3 config

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-const express = require('express') 
-const app = express()
-const router = express.Router()
-const mongoose = require('mongoose')
-const config = require('../config/key');
-const port = 3000 
-const bodyParser = require('body-parser');
-const { Subscribe } = require('./models/subscribe') 
-const { User } = require('./models/model') 
-const { Like } = require('./models/model') 
-const { Review } = require('./models/model') 
-const { Book } = require('./models/model') 
-const { Quote } = require('./models/model')
-const userRouter = require('./routes/user')
-const cookieParser = require('cookie-parser')
-const session_key = require('../config/session_key');
-const session = require('express-session')
-const fileStore = require('session-file-store')(session);
+const express = require("express");
+const app = express();
+const router = express.Router();
+const mongoose = require("mongoose");
+const config = require("../config/key");
+const port = 3000;
+const bodyParser = require("body-parser");
+const { Subscribe } = require("./models/subscribe");
+const { User } = require("./models/model");
+const { Like } = require("./models/model");
+const { Review } = require("./models/model");
+const { Book } = require("./models/model");
+const { Quote } = require("./models/model");
+const userRouter = require("./routes/user");
+const bookRouter = require("./routes/book");
+const cookieParser = require("cookie-parser");
+const session_key = require("../config/session_key");
+const session = require("express-session");
+const fileStore = require("session-file-store")(session);
 
-app.use(session({
+app.use(
+  session({
     httpOnly: true,
     secure: true,
     // secret: session_key.secret_key,
@@ -25,27 +27,32 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: {
-        httpOnly: true,
-        maxAge:(3.6e+6)*24 // 24시간 유효
+      httpOnly: true,
+      maxAge: 3.6e6 * 24, // 24시간 유효
     },
-    store: new fileStore()
+    store: new fileStore(),
     // store: MongoStore.create({
     //     mongoUrl: config.mongoURI
     // })
-}))
-app.use(cookieParser())
+  })
+);
+app.use(cookieParser());
 
-mongoose.connect(config.mongoURI, {
-	useNewUrlParser: true, useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected ...'))
-	.catch(err => console.log(err))
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected ..."))
+  .catch((err) => console.log(err));
 
-app.use(bodyParser.urlencoded({extended: true})); 
-app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!')) 
+app.get("/", (req, res) => res.send("Hello World!"));
 
 // join
-app.use('/user', userRouter)
+app.use("/user", userRouter);
+app.use("/book", bookRouter);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/routes/book.js
+++ src/routes/book.js
@@ -1,25 +1,50 @@
-const express = require('express') 
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const upload = require("../aws-storage.js");
+const { Book } = require("../models/model");
 
-router.get('/most-read', (req, res) => {
-    res.send("most-read");
+router.get("/most-read", (req, res) => {
+  res.send("most-read");
 });
 
-router.post('/add-book', (req, res) => {
-    res.send("add-book");
+router.post("/add-book", upload.single("bookImage"), async (req, res) => {
+  console.log("req:", req.file.location);
+  try {
+    const bookInfo = new Book({
+      isbn: req.body.isbn,
+      title: req.body.title,
+      page: req.body.page,
+      writer: req.body.writer,
+      translator: req.body.translator,
+      publisher: req.body.publisher,
+      category: req.body.category,
+      field: req.body.title,
+      bookImage: req.file.location,
+    });
+
+    console.log(bookInfo);
+
+    const result = await bookInfo.save();
+
+    res.status(200).json({
+      join_success: true,
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Server error", err });
+  }
 });
 
-router.post('/add-review', (req, res) => {
-    res.send("add-review");
+router.post("/add-review", (req, res) => {
+  res.send("add-review");
 });
 
-router.get('/info', (req, res) => {
-    res.send("info");
+router.get("/info", (req, res) => {
+  res.send("info");
 });
 
-router.get('/info/rewiew', (req, res) => {
-    res.send("info/rewiew");
+router.get("/info/rewiew", (req, res) => {
+  res.send("info/rewiew");
 });
 
-
-module.exports = router
+module.exports = router;

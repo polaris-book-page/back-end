8465e9de7458aa851d2e3a4406283f8b8cb7002c
kjh073 <9279482@naver.com>
2023-08-18 00:20:57 +0900

    [feat] Implement loading star review/like list & add/delete like book - implement loading star review - implement add like book - implement delete like book - implement loading like list

--- src/routes/mypage.js
+++ src/routes/mypage.js
@@ -1,5 +1,6 @@
 const express = require('express') 
 const router = express.Router()
+const { Review, Like, Book } = require('../models/model')
 
 router.get('/', (req, res) => {
     res.send("mypage");
@@ -17,8 +18,29 @@ router.get('/calendar', (req, res) => {
     res.send("calendar");
 });
 
-router.post('/star-review', (req, res) => {
-    res.send("star-review");
+router.post('/star-review', async (req, res) => {
+    if(!req.session.is_logined){
+        res.redirect('/user/login');
+    }
+    try {
+        const results = await Review.find({ userId: req.session.userId })
+        if (results.length === 0) {
+            res.status(404).json({ 
+                findMyReview: false, 
+                message: 'No review corresponding to this user' 
+            });
+        }
+        const reviewList = results.map(result => ({
+            userId: result.userId,
+            isbn: result.isbn,
+            evaluation: result.evaluation,
+            endDate: result.endDate
+        }));
+        res.status(200).json(reviewList);
+    } catch (err) {
+        console.error('Error in read my review list', err);
+        res.status(500).json({ findMyReview: false, err });
+    }
 });
 
 router.post('/star-review/detail', (req, res) => {
@@ -33,12 +55,66 @@ router.delete('/review/delete', (req, res) => {
     res.send("review/delete");
 });
 
-router.post('/like', (req, res) => {
-    res.send("like");
+router.post('/like', async (req, res) => {
+    if(!req.session.is_logined){
+        res.redirect('/user/login');
+    }
+    const likeInfo = new Like(req.body);
+    likeInfo.userId = req.session.userId
+    try {
+        const result = await likeInfo.save();
+        
+        res.status(200).json({
+            like_success: true,
+            result: result 
+        });
+    } catch (err) {
+        console.error('Error in save like book', err);
+        res.status(500).json({ like_success: false, err });
+    }
 });
 
-router.delete('/unlike', (req, res) => {
-    res.send("unlike");
+router.delete('/unlike', async (req, res) => {
+    if(!req.session.is_logined){
+        res.redirect('/user/login');
+    }
+    try {
+        const result = await Like.deleteOne({ userId: req.session.userId, isbn: req.body.isbn });
+        res.status(200).json({
+            likeDel_success: true,
+            result: result 
+        });
+    } catch (err) {
+        console.error('Error in delete like book', err);
+        res.status(500).json({ likeDel_success: false, err });
+    }
 });
+
+router.get('/like/list', async (req, res) => {
+    if(!req.session.is_logined){
+        res.redirect('/user/login');
+    }
+    try {
+        const likes = await Like.find({ userId: req.session.userId });
+        const isbn = likes.map(like => like.isbn);
+        const books = await Book.find({ isbn: isbn });
+        const result = likes.map(like => {
+            const book = books.find(book => book.isbn.toString() === like.isbn.toString());
+            return {
+                _id: like._id,
+                isbn: like.isbn,
+                userId: like.userId,
+                title: book ? book.title : null,
+                writer: book ? book.writer : null,
+                publisher: book ? book.publisher : null,
+                bookImage: book ? book.bookImage : null,
+            };
+        })
+        res.status(200).json(result)
+    } catch (err) {
+        console.error('Error in read like list', err);
+        res.status(500).json({ findLike: false, err });
+    }
+})
 
 module.exports = router

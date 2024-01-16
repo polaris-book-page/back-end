fa90732b3d183aa8f906e47ab4cc4223afad45d3
kjh073 <9279482@naver.com>
2024-01-16 23:07:20 +0900

    [feat] Implement initial-evaluation router - implement initial-evaluation router

--- src/routes/user.js
+++ src/routes/user.js
@@ -73,8 +73,42 @@ router.get('/join/nickname-check', async(req, res) => {
     }
 });
 
-router.post('/initial-evaluation', (req, res) => {
-    res.send("/initial-evaluation");
+router.post('/initial-evaluation', async(req, res) => {
+    const books = req.body;
+    try {
+        const bookList = books.map(async book => {
+            // 별점 저장
+            const evaluatedBook = new Review({
+                userId: req.session.userId,
+                isbn: book.isbn13,
+                evaluation: book.evaluation
+            })
+            // 책 정보 저장
+            const haveTranslator = book.author.indexOf('(옮긴이)')
+            const catagory = book.categoryName.substring(book.categoryName.indexOf('>') + 1)
+            const bookInfo = new Book({
+                isbn: book.isbn13,
+                title: book.title,
+                writer: book.author.substring(0, book.author.indexOf('(지은이)') - 1),
+                translator: haveTranslator != -1 
+                ? book.author.substring(book.author.indexOf(',') + 2, book.author.indexOf('(옮긴이)') - 1) 
+                : null,
+                publisher: book.publisher, 
+                //카테고리 > 분야
+                category: catagory.substring(0, catagory.indexOf('>')),
+                field: catagory.substring(catagory.indexOf('>') + 1, catagory.indexOf('>', catagory.indexOf('>') + 1)), //slice필요
+                bookImage: book.cover
+            })
+            await evaluatedBook.save()
+            return await bookInfo.save()
+            
+        })
+        const result = await Promise.all(bookList);
+        res.status(200).json(result);
+    } catch(err) {
+        console.error('Error in save evaluatedBooks', err);
+        res.status(500).json({ saveEvaluatedBooks: false, err });
+    }
 });
 
 router.post('/login', async (req, res) => {

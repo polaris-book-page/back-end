748d29f99f492f61309ec6db9ee8251e7f8bede6
kjh073 <9279482@naver.com>
2024-03-31 00:35:22 +0900

    [fix] Resolve bug field name not saved correctly - resolve bug field name not saved correctly

--- src/routes/search.js
+++ src/routes/search.js
@@ -4,14 +4,19 @@ const { Book } = require('../models/model');
 
 router.post('/result/save', async (req, res) => {
     const { books } = req.body;
-
+    
     try {
         const bookList = books.map(async book => {
             const alreadyHave13 = await Book.findOne({ isbn: book.isbn13.toString() })
             const alreadyHave = await Book.findOne({ isbn: book.isbn.toString() })
             const haveTranslator = book.author.indexOf('(옮긴이)')
+            const catagory = book.categoryName.substring(book.categoryName.indexOf('>') + 1)
+            if (catagory.indexOf('>', catagory.indexOf('>') + 1) !== -1) {
+                lastIndex = catagory.indexOf('>', catagory.indexOf('>') + 1)
+            } else {
+                lastIndex = catagory.length
+            }
             if (!(alreadyHave || alreadyHave13)) {
-                const catagory = book.categoryName.substring(book.categoryName.indexOf('>') + 1)
                 const newBook = new Book({
                     isbn: book.isbn13 ? book.isbn13 : book.isbn,
                     title: book.title,
@@ -22,7 +27,7 @@ router.post('/result/save', async (req, res) => {
                     publisher: book.publisher, 
                     //카테고리 > 분야
                     category: catagory.substring(0, catagory.indexOf('>')),
-                    field: catagory.substring(catagory.indexOf('>') + 1, catagory.indexOf('>', catagory.indexOf('>') + 1)), //slice필요
+                    field: catagory.substring(catagory.indexOf('>') + 1, lastIndex), 
                     bookImage: book.cover
                 })
                 return await newBook.save()
